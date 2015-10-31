122366c5e0c97aba4358d869ebb58636876508f4
tedcurrent <tetsiiedm@gmail.com>
2015-10-31 16:05:29 +0200

    regexing

--- PasswordChecker.playground/Contents.swift
+++ PasswordChecker.playground/Contents.swift
@@ -3,14 +3,23 @@
 //  x   not empty
 //  x   minimum 6 characters
 //  x   maximum 16 characters
-//      at least one capital letter
-//      at least one non-capital letter
-//      at least one number
-//      at least one character
+//  x   at least one capital letter
+//  x   at least one non-capital letter
+//  x   at least one number
+//  x   at least one character
 //      Bonus:
-//      at least one special character
+//  x   at least one special character
+//  x   without a whitespace
 
 import Foundation
+
+// Short-hand regex extension
+extension NSRegularExpression {
+    func generalTest(toBeTested str: String) -> NSTextCheckingResult? {
+        let range = NSMakeRange(0, str.characters.count)
+        return self.firstMatchInString(str, options: .WithTransparentBounds, range: range)
+    }
+}
 
 // The possible error types for password
 enum PasswordFail: ErrorType {
@@ -18,6 +27,11 @@ enum PasswordFail: ErrorType {
     case MaximumLength
     case Empty
     case Uppercase
+    case Lowercase
+    case Number
+    case Character
+    case Special
+    case WhiteSpace
 }
 
 class PasswordChecker {
@@ -36,7 +50,17 @@ class PasswordChecker {
         } catch PasswordFail.MaximumLength {
             err = "Your password does not meet the maximum character requirement of 16."
         } catch PasswordFail.Uppercase {
-            err = "Your password has no uppercase characters."
+            err = "Your password has to include at least one uppercase character."
+        } catch PasswordFail.Lowercase {
+            err = "Your password has to include at least one lowercase character."
+        } catch PasswordFail.Number {
+            err = "Your password has to include at least one numeric character."
+        } catch PasswordFail.Character {
+            err = "Your password has to include at least one word character."
+        } catch PasswordFail.Special {
+            err = "Your password has to include at least one special character."
+        } catch PasswordFail.WhiteSpace {
+            err = "Your password cannot include a whitespace, a tab space or a line break."
         } catch {
             err = "Unknown error!"
         }
@@ -58,16 +82,41 @@ class PasswordChecker {
         guard pw.lowercaseString != pw else {
             throw PasswordFail.Uppercase
         }
+        guard pw.uppercaseString != pw else {
+            throw PasswordFail.Lowercase
+        }
+        var regex = try! NSRegularExpression(pattern: "\\d", options: .CaseInsensitive)
+        guard regex.generalTest(toBeTested: pw) != nil else {
+            throw PasswordFail.Number
+        }
+        regex = try! NSRegularExpression(pattern: "[A-Za-z]", options: .CaseInsensitive)
+        guard regex.generalTest(toBeTested: pw) != nil else {
+            throw PasswordFail.Character
+        }
+        regex = try! NSRegularExpression(pattern: "[^\\w]", options: .CaseInsensitive)
+        guard regex.generalTest(toBeTested: pw) != nil else {
+            throw PasswordFail.Special
+        }
+        regex = try! NSRegularExpression(pattern: "[\\s]", options: .CaseInsensitive)
+        guard regex.generalTest(toBeTested: pw) == nil else {
+            throw PasswordFail.WhiteSpace
+        }
     }
 }
 
 let passwordChecker = PasswordChecker()
-passwordChecker.checkPw(password: "m123215M") {goodPassword, error in
+passwordChecker.checkPw(password: "maaaa2M@") {goodPassword, error in
     if goodPassword != nil {
         print("Yay, the password was ok and now I can do something with it")
     } else {
         print(error!)
     }
 }
+
+let regex = try! NSRegularExpression(pattern:"[\\s]", options: .CaseInsensitive)
+if regex.generalTest(toBeTested: "0101") != nil {
+    print("gotcha")
+}
+
 
 
